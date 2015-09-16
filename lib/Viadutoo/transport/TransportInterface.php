1874ea4510db3e2303b57ac72dd6806f469338d7
Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2015-09-16 13:39:15 -0400

    TLCALIPER-16 - Updated interface to specify parameter setting methods and different arguments for send()

--- lib/Viadutoo/transport/TransportInterface.php
+++ lib/Viadutoo/transport/TransportInterface.php
@@ -2,8 +2,26 @@
 
 interface TransportInterface {
     /**
-     * @param Proxy $proxy
-     * @return bool
+     * @param string $endpointUrl
+     * @return $this
      */
-    public function send($proxy);
+    public function setEndpointUrl($endpointUrl);
+
+    /**
+     * The number of seconds to wait for the data to be sent.
+     *
+     * Fractions of a second, down to the millisecond, may be specified.  Setting this to
+     * null would use the default timeout value specified by the transport.
+     *
+     * @param float|null $timeoutSeconds
+     * @return $this
+     */
+    public function setTimeoutSeconds($timeoutSeconds);
+
+    /**
+     * @param string[] $headers
+     * @param string $body
+     * @return int|null HTTP response code
+     */
+    public function send($headers, $body);
 }
