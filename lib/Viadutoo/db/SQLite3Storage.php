4f6b4d68544594c7c44c5afbb74afd024e01f80d
Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2015-09-18 12:16:38 -0400

    TLCALIPER-15 -  Added optional table name and check for good connection.

--- lib/Viadutoo/db/SQLite3Storage.php
+++ lib/Viadutoo/db/SQLite3Storage.php
@@ -4,15 +4,22 @@
 class SQLite3Storage implements StorageInterface {
     /** @var SQLite3 */
     private $_databaseHandle;
+    /** @var string */
+    private $_tableName;
 
     /**
      * @param string $databaseFilename
      */
-    public function __construct($databaseFilename) {
+    public function __construct($databaseFilename, $tableName = 'events') {
+        $this->_tableName = strval($tableName);
         $this->_databaseHandle = new SQLite3($databaseFilename);
 
-        $this->_databaseHandle->exec(<<<'EOT'
-            CREATE TABLE IF NOT EXISTS events (
+        if ($this->_databaseHandle->lastErrorCode() != 0) {
+            throw new Exception('Unable to connect to DB.');
+        }
+
+        $this->_databaseHandle->exec(<<<"EOT"
+            CREATE TABLE IF NOT EXISTS $tableName (
                 id INTEGER PRIMARY KEY,
                 headers STRING,
                 body STRING
@@ -27,8 +34,9 @@ EOT
      * @return SQLite3Result
      */
     public function store($headers, $body) {
+        $tableName = $this->_tableName;
         $statement = $this->_databaseHandle
-            ->prepare('INSERT INTO events (id, headers, body) VALUES (null, :headers, :body)');
+            ->prepare("INSERT INTO $tableName (id, headers, body) VALUES (null, :headers, :body)");
         $statement->bindValue(':headers', json_encode($headers), SQLITE3_TEXT);
         $statement->bindValue(':body', $body, SQLITE3_TEXT);
         $result = $statement->execute();
