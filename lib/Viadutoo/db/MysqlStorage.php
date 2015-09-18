bad6b425c450ce467f9004c0717910db6f743520
Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2015-09-18 12:17:12 -0400

    TLCALIPER-15 -  Added optional table name and SQL to create table if it doesn't exist.

--- lib/Viadutoo/db/MysqlStorage.php
+++ lib/Viadutoo/db/MysqlStorage.php
@@ -3,13 +3,25 @@
 class MysqlStorage implements StorageInterface{
     /** @var mysqli */
     private $_databaseHandle;
+    /** @var string */
+    private $_tableName;
 
-    function __construct($host, $userName, $password, $dbName) {
+    function __construct($host, $userName, $password, $dbName, $tableName = 'events') {
+        $this->_tableName = strval($tableName);
         $this->_databaseHandle = new mysqli($host, $userName, $password, $dbName);
 
         if ($this->_databaseHandle->connect_error) {
             throw new Exception('Unable to connect to DB.');
         }
+
+        $this->_databaseHandle->query(<<<"EOT"
+            CREATE TABLE IF NOT EXISTS $tableName (
+                id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                headers text NOT NULL,
+                body text NOT NULL
+            )
+EOT
+        );
     }
 
     /**
@@ -18,9 +30,10 @@ class MysqlStorage implements StorageInterface{
      * @return mixed
      */
     function store($headers, $body) {
+        $tableName = $this->_tableName;
         $encodedHeaders = json_encode($headers);
         $statement = $this->_databaseHandle
-            ->prepare('INSERT INTO caliper_events (id, headers, body) VALUES (null, ?, ?)');
+            ->prepare("INSERT INTO $tableName (id, headers, body) VALUES (null, ?, ?)");
         $statement->bind_param('ss', $encodedHeaders, $body);
         $result = $statement->execute();
 
